729cb6c69de0b9b11ad936ee0609a9b4204a0cb5
mbbous <mbbous@gmail.com>
2021-01-24 11:45:00 -0400

    added controller dir and moved the index controler

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+let index = require('../contollers/index');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', index.index);
 
 module.exports = router;
+ 
